c03fd31ad702b6cb6764f007b855c54245301f92
shahbajahmed7866u <shahbajahmed7866u@gmail.com>
2022-05-07 19:07:11 +0530

    some files

---
+++ cart.js
@@ -0,0 +1,7 @@
+var obj = JSON.parse(localStorage.getItem("signupData"))
+document.querySelector("#SignUpName").innerText="Hi,"+obj.fname+obj.lname || 'Sign Up'
+
+if(document.querySelector("#SignUpName").innerText=="Hi,"+obj.fname+obj.lname){
+    document.querySelector("#SignUpName").href="#"
+  
+}
